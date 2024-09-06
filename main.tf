f560de4472272861a43998071ea537c8145397c6
AnandJoy7 <88574279+AnandJoy7@users.noreply.github.com>
2024-09-06 13:51:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,6 @@ provider "aws" {
 
 resource "aws_s3_bucket" "example_bucket" {
   bucket = "ducket1"  # Replace with a unique bucket name
-  acl    = "private"
 
   tags = {
     Name        = "ExampleS3Bucket"
