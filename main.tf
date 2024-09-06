0469ff9b909ce849fcdfedcfbb4118a15e014aa0
AnandJoy7 <88574279+AnandJoy7@users.noreply.github.com>
2024-09-06 14:24:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,3 @@
-
-provider "aws" {
-  region = "us-east-1"  # Replace with your preferred region
-}
-
 resource "aws_s3_bucket" "example_bucket" {
   bucket = "ducket1"  # Replace with a unique bucket name
 
